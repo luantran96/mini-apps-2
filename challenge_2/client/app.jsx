6f1597292c088bcfc41ed37220c9c64d727d68a2
Luan Tran <luan.phuoc.tran.96@gmail.com>
2018-12-06 12:12:35 -0800

    bitcoin prices are shown correctly

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,23 +1,75 @@
 import React from 'react';
 import axios from 'axios';
+import Chart from 'chart.js';
 
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = ({
-    
+          data: undefined,
         });
+
+        this.renderChart = this.renderChart.bind(this);
+    }
+    
+    componentDidMount() {
+      axios.get('/api/btc', {
+        params: {
+          start_date: '2013-09-01',
+          end_date: '2018-09-01',
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          data: res.data,
+        })
+      });
     }
 
-    componentDidMount() {
+    renderChart() {
+      const { data } = this.state;
+      if (!data) {
+        return;
+      }
 
+      console.log(data.bpi);
+      let dates = Object.keys(data.bpi);
+      let prices = Object.values(data.bpi);
+
+      var ctx = document.getElementById("myChart");
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: dates,
+            datasets: [{
+                label: 'Bitcoin prices',
+                data: prices,
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                ],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            },
+            lineTension: 2,
+        },
+      });
     }
 
     render() {
         return (
             <div>
-                HELLO WORLD
+                <canvas id="myChart" width="500" height="500"></canvas>
+                {this.renderChart()}
             </div>
         )
     }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -7,16 +7,14 @@ const app = express();
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/api/', (req, res) => {
-
+app.get('/api/btc', (req, res) => {
     const { start_date, end_date } = req.query;
-    
     var options = {
       url: `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start_date}&end=${end_date}`,
     };
 
     request(options, (err, response, body) => {
-      if (!error && response.statusCode == 200) {
+      if (!err && response.statusCode == 200) {
         var info = JSON.parse(body);
         res.json(info);
       }
